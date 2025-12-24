105b50896b1b2003e8055ec10422727266a6547b
Kin Hei Shing <shingkinhei@gmail.com>
2025-12-24 00:44:21 +0000

    main

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'rodtfkraukblqbshlazo.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
