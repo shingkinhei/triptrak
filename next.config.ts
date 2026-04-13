420f8d2ae9a06fe887bbb850fd00690e5f870da9
shingkinhei <shingkinhei@gmail.com>
2026-04-13 20:58:06 +0800

    PWA test

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  //swMinify: true,
+  disable: process.env.NODE_ENV === "development",
+
+  workboxOptions: {
+    runtimeCaching: [
+      {
+
+        urlPattern: /^https:\/\/.*\.supabase\.co\/rest\/v1\/.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'supabase-api-cache',
+          expiration: {
+            maxEntries: 100,
+            maxAgeSeconds: 60 * 60 * 24 * 7, // 7 days
+          },
+        },
+      },
+    ],
+  },
+});
 
 const nextConfig: NextConfig = {
   typescript: {
@@ -40,4 +67,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(withPWA(nextConfig));
